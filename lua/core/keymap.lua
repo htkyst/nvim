245042fe756c636db90c97a550ea2afc9418b02a
htkyst-laptop <rubybox@outlook.jp>
2024-04-10 22:38:22 +0900

    add neogen

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,8 +1,8 @@
 -- Key mappings
-vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', { silent = true })                              -- ESCキーでターミナルモードからノーマルモードに移行
-vim.keymap.set('n', 'tt', '<cmd>belowright new<CR><cmd>terminal<CR>', { silent = true })    -- ttで下分割でターミナルを起動
-vim.keymap.set('n', 'bn', ':bn<CR>', { silent = true })                                     -- bnで次のバッファへ移動
-vim.keymap.set('n', 'bp', ':bp<CR>', { silent = true })                                     -- bpで前のバッファへ移動
+vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', { silent = true })                           -- ESCキーでターミナルモードからノーマルモードに移行
+vim.keymap.set('n', 'tt', '<cmd>belowright new<CR><cmd>terminal<CR>', { silent = true }) -- ttで下分割でターミナルを起動
+vim.keymap.set('n', 'bn', ':bn<CR>', { silent = true })                                  -- bnで次のバッファへ移動
+vim.keymap.set('n', 'bp', ':bp<CR>', { silent = true })                                  -- bpで前のバッファへ移動
 
 -- Leader + h/j/k/l でペイン移動
 vim.keymap.set('n', '<leader>h', '<C-w>h', { silent = true })
@@ -15,4 +15,3 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
