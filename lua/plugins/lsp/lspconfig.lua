5185b6ec9d7b21d5e09b0ee668fe093d871366f3
htkyst <rubybox@outlook.jp>
2024-10-21 23:11:25 +0900

    fix lsp

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,20 +1,5 @@
 return {
 	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-
-	{
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
 			{ "neovim/nvim-lspconfig" },
