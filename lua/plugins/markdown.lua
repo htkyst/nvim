88f5ad0b4b4411db0452af815c4e6b1b77bb5f10
htkyst-laptop <rubybox@outlook.jp>
2025-12-13 18:59:49 +0900

    minor fixes

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,10 +1,14 @@
 return {
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        cond = function()
+            return vim.fn.executable("yarn") == 1
+        end,
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    }
 }
