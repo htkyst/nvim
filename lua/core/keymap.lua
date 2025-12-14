59e3d97725b3bd3cb5c9646579ca6c03109173d6
rp4htkyst <rubybox@outlook.jp>
2025-12-14 19:51:54 +0900

    Delete keymap about move line

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -19,7 +19,3 @@ vim.keymap.set("n", "<leader>l", "<C-w>l", { silent = true })
 -- Split pane
 vim.keymap.set("n", "ss", ":split<Return><C-w>w")
 vim.keymap.set("n", "vv", ":vsplit<Return><C-w>w")
-
--- Move to line begin and end
-vim.keymap.set("n", "0", "$")
-vim.keymap.set("n", "1", "0")
