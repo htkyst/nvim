33c167d545896807a544923c54715ce38fb63718
htkyst-laptop <rubybox@outlook.jp>
2026-01-24 15:08:32 +0900

    Fix LSP configuration for Neovim breaking changes

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,62 +1,69 @@
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		version = "^1.0.0",
-		dependencies = {
-			{ "neovim/nvim-lspconfig" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "simrat39/rust-tools.nvim", ft = "rs" },
-		},
-		config = function()
-			local mason_lspconfig = require("mason-lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason-lspconfig.nvim",
+    version = "^1.0.0",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local mason_lspconfig = require("mason-lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local ensure_installed = {
-				"lua_ls",
-				"rust_analyzer",
-				"clangd",
-				-- "csharp_ls",
-				-- "html", -- html-lsp
-				-- "cssls", -- css-lsp
-				-- "ts_ls", -- typescript-language-server
-				-- "pyright",
-			}
+      local servers = {
+        "lua_ls",
+        "rust_analyzer",
+        "clangd",
+      }
 
-			mason_lspconfig.setup({
-				automatic_installation = true,
-				ensure_installed = ensure_installed,
-			})
+      mason_lspconfig.setup({
+        ensure_installed = servers,
+        automatic_installation = true,
+      })
 
-			vim.lsp.config("*", {
-				capabilities = capabilities,
-			})
+      -- lua_ls
+      vim.lsp.config.lua_ls = {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              version = "LuaJIT",
+            },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+              },
+            },
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      }
 
-			-- lua_ls
-			vim.lsp.config("lua_ls", {
-				settings = {
-					Lua = {
-						runtime = {
-							version = "LuaJIT",
-						},
-						workspace = {
-							checkThirdParty = false,
-							library = {
-								vim.env.VIMRUNTIME,
-							},
-						},
-						diagnostics = {
-							globals = { "vim" },
-						},
-					},
-				},
-			})
+      -- rust_analyzer
+      vim.lsp.config.rust_analyzer = {
+        capabilities = capabilities,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+            },
+            checkOnSave = {
+              command = "clippy",
+            },
+          },
+        },
+      }
 
-			-- rust_analyzer
-			vim.lsp.config("rust_analyzer", {
-				require("rust-tools").setup({}),
-			})
+      -- clangd
+      vim.lsp.config.clangd = {
+        capabilities = capabilities,
+      }
 
-			vim.lsp.enable(ensure_installed)
-		end,
-	},
+      vim.lsp.enable(servers)
+    end,
+  },
 }
+
